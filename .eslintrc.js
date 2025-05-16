70dd80c3426744efac2cacb4a01c94cf369f0211
johnkim1216 <johnmjkim1216@gmail.com>
2025-05-16 11:16:21 +1000

    add ES lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,18 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "react-app",
+    "react-app/jest"
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   rules: {
-    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'react/prop-types': 'off', // Not needed with TypeScript
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-mixed-operators": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "no-useless-escape": "error",
+    "import/no-unused-modules": "error"
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
+      version: "detect"
+    }
+  }
 }; 
